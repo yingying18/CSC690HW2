42462a323b80b4173bbc16b13e2caccdac62585c
Jasmine Chen <milktea@jasmines-imac.local>
2018-10-24 00:45:34 -0700

    Initial Commit

---
+++ ToDoApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ToDoApp
+//
+//  Created by Jasmine Chen on 10/24/18.
+//  Copyright © 2018 Jasmine Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
